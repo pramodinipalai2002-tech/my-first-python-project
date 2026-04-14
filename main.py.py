4420adba168c901952dd7a3862339ed011d6cb61
pramodinipalai2002-tech <pramodinipalai2002@gmail.com>
2026-04-14 18:35:36 +0530

    This is my first python project
    Features:
    -Takes user input
    -Shows output
    -Uses basic logic(if-else)

---
+++ main.py.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello",name)
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
